e45f689e1f22f149ab1470b7a41a66557d074049
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-07-02 14:46:36 +0300

    Update FoodsController.cs

--- Web/MyPetProject.Web/Controllers/FoodsController.cs
+++ Web/MyPetProject.Web/Controllers/FoodsController.cs
@@ -1,27 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MyPetProject.Web.ViewModels.Foods;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MyPetProject.Web.Controllers
+﻿namespace MyPetProject.Web.Controllers
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using MyPetProject.Data;
+    using MyPetProject.Data.Models;
+
     public class FoodsController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        [BindProperty]
+        public Food Food { get; set; }
+
+        public FoodsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
         public IActionResult Index()
         {
             return this.View();
         }
 
-        public IActionResult Create()
+        public IActionResult Create(int? id)
         {
-            return this.View();
+            this.Food = new Food();
+            if (id == null)
+            {
+                return this.View(this.Food);
+            }
+
+            this.Food = this.db.Foods.FirstOrDefault(d => d.Id == id);
+            if (this.Food == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(this.Food);
         }
 
-        /*[HttpPost]
-        public Task<IActionResult> Create(FoodViewModel inputModel)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create()
         {
-        }*/
+            // Dino = new Dino();
+            if (this.ModelState.IsValid)
+            {
+                if (this.Food.Id == 0)
+                {
+                    this.db.Foods.Add(this.Food);
+                }
+                else
+                {
+                    this.db.Foods.Update(this.Food);
+                }
+
+                this.db.SaveChanges();
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(this.Food);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return this.Json(new { data = await this.db.Foods.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var foods = await this.db.Foods.FirstOrDefaultAsync(u => u.Id == id);
+            if (foods != null)
+            {
+                this.db.Foods.Remove(foods);
+                await this.db.SaveChangesAsync();
+                return this.Json(new { success = true, message = "Delete successful" });
+            }
+
+            return this.Json(new { success = false, message = "Error while Deleting" });
+        }
     }
 }
