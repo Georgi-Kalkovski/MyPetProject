fed40d8fad6743675b25ec880f8b31b06e0cbdfa
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-07-02 13:06:49 +0300

    Start of my project - MyPetProject

---
+++ Data/MyPetProject.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPetProject.Data.Models
+{
+    using MyPetProject.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
