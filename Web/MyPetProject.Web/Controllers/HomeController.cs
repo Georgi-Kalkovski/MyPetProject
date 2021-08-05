5aa7dca6c455bb79dabf0e670c19a2a3a2515831
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2021-08-05 16:08:53 +0300

    Adding Search view

--- Web/MyPetProject.Web/Controllers/HomeController.cs
+++ Web/MyPetProject.Web/Controllers/HomeController.cs
@@ -1,12 +1,30 @@
 ﻿namespace MyPetProject.Web.Controllers
 {
     using System.Diagnostics;
-
+    using System.Linq;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using MyPetProject.Data.Common.Repositories;
+    using MyPetProject.Data.Models;
     using MyPetProject.Web.ViewModels;
 
     public class HomeController : BaseController
     {
+        private readonly IDeletableEntityRepository<Kingdom> kingdomsRepository;
+        private readonly IDeletableEntityRepository<Breed> breedsRepository;
+        private readonly IDeletableEntityRepository<ApplicationUser> usersRepository;
+
+        public HomeController(
+            IDeletableEntityRepository<Kingdom> kingdomsRepository,
+            IDeletableEntityRepository<Breed> breedsRepository,
+            IDeletableEntityRepository<ApplicationUser> usersRepository)
+        {
+            this.kingdomsRepository = kingdomsRepository;
+            this.breedsRepository = breedsRepository;
+            this.usersRepository = usersRepository;
+        }
+
         public IActionResult Index()
         {
             return this.View();
@@ -20,6 +38,15 @@
         public IActionResult Groups()
         {
             return this.View();
+        }
+
+        public async Task<IActionResult> SearchAsync()
+        {
+            var result = this.kingdomsRepository
+                .All()
+                .OrderBy(x => x.Name);
+
+            return this.View(await result.ToListAsync());
         }
 
         public IActionResult ErrorPage()
