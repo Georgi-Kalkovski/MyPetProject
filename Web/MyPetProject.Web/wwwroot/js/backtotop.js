008c174724b54c42f242e19e8ecb066327170172
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2021-08-09 01:42:28 +0300

    Update backtotop.js

--- Web/MyPetProject.Web/wwwroot/js/backtotop.js
+++ Web/MyPetProject.Web/wwwroot/js/backtotop.js
@@ -1,14 +1,17 @@
 ﻿//Get the button:
-mybutton = document.getElementById("back-to-top");
+mybutton = document.getElementById("backToTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
+    console.log(mybutton)
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
+        console.log("check")
     } else {
         mybutton.style.display = "none";
+        console.log("uncheck")
     }
 }
 
