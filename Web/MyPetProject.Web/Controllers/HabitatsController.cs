a30c4500246b13be68e7c451b860a315d4bf913c
Georgi Kalkovski <gopeto.92@gmail.com>
2021-08-18 16:52:05 +0300

    Moved Habitats actions/views to their own Controller/View folders.

---
+++ Web/MyPetProject.Web/Controllers/HabitatsController.cs
@@ -0,0 +1,75 @@
+﻿namespace MyPetProject.Web.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using MyPetProject.Data.Common.Repositories;
+    using MyPetProject.Data.Models;
+
+    public class HabitatsController : BaseController
+    {
+        private readonly IDeletableEntityRepository<Kingdom> kingdomsRepository;
+
+        public HabitatsController(
+            IDeletableEntityRepository<Kingdom> kingdomsRepository)
+        {
+            this.kingdomsRepository = kingdomsRepository;
+        }
+
+        // GET: /HomeAnimals
+        [HttpGet("/HomeAnimals")]
+        public async Task<IActionResult> HomeAnimals()
+        {
+            return await this.HomeAnimalsMethod();
+        }
+
+        // GET: /FarmAnimals
+        [HttpGet("/FarmAnimals")]
+        public async Task<IActionResult> FarmAnimals()
+        {
+            return await this.FarmAnimalsMethod();
+        }
+
+        // GET: /WildAnimals
+        [HttpGet("/WildAnimals")]
+        public async Task<IActionResult> WildAnimals()
+        {
+            return await this.WildAnimalMethod();
+        }
+
+        private async Task<IActionResult> HomeAnimalsMethod()
+        {
+            var result = this.kingdomsRepository
+                            .All()
+                            .Include(k => k.User)
+                            .Where(a => a.IsPet == true)
+                            .OrderBy(x => x.Name);
+
+            return this.View(await result.ToListAsync());
+        }
+
+        private async Task<IActionResult> FarmAnimalsMethod()
+        {
+            var result = this.kingdomsRepository
+                            .All()
+                            .Include(k => k.User)
+                            .Where(a => a.IsFarm == true)
+                            .OrderBy(x => x.Name);
+
+            return this.View(await result.ToListAsync());
+        }
+
+        private async Task<IActionResult> WildAnimalMethod()
+        {
+            var result = this.kingdomsRepository
+                            .All()
+                            .Include(k => k.User)
+                            .Where(a => a.IsPet == false && a.IsFarm == false)
+                            .OrderBy(x => x.Name);
+
+            return this.View(await result.ToListAsync());
+        }
+    }
+}
--- Web/MyPetProject.Web/Controllers/HomeController.cs
+++ Web/MyPetProject.Web/Controllers/HomeController.cs
@@ -60,27 +60,6 @@
             return this.View();
         }
 
-        // GET: /HomeAnimals
-        [HttpGet("/HomeAnimals")]
-        public async Task<IActionResult> HomeAnimals()
-        {
-            return await this.HomeAnimalsMethod();
-        }
-
-        // GET: /FarmAnimals
-        [HttpGet("/FarmAnimals")]
-        public async Task<IActionResult> FarmAnimals()
-        {
-            return await this.FarmAnimalsMethod();
-        }
-
-        // GET: /WildAnimals
-        [HttpGet("/WildAnimals")]
-        public async Task<IActionResult> WildAnimals()
-        {
-            return await this.WildAnimalMethod();
-        }
-
         public IActionResult ErrorPage()
         {
             return this.View();
@@ -91,39 +70,6 @@
         {
             return this.View(
                 new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
-        }
-
-        private async Task<IActionResult> HomeAnimalsMethod()
-        {
-            var result = this.kingdomsRepository
-                            .All()
-                            .Include(k => k.User)
-                            .Where(a => a.IsPet == true)
-                            .OrderBy(x => x.Name);
-
-            return this.View(await result.ToListAsync());
-        }
-
-        private async Task<IActionResult> FarmAnimalsMethod()
-        {
-            var result = this.kingdomsRepository
-                            .All()
-                            .Include(k => k.User)
-                            .Where(a => a.IsFarm == true)
-                            .OrderBy(x => x.Name);
-
-            return this.View(await result.ToListAsync());
-        }
-
-        private async Task<IActionResult> WildAnimalMethod()
-        {
-            var result = this.kingdomsRepository
-                            .All()
-                            .Include(k => k.User)
-                            .Where(a => a.IsPet == false && a.IsFarm == false)
-                            .OrderBy(x => x.Name);
-
-            return this.View(await result.ToListAsync());
         }
 
         private void TodayRandomEntityMethod()
